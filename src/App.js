e5e0691e2f2e7eb15bd6cf2c80015a1176cb17f3
Vania Tavares <vntavares56@gmail.com>
2022-04-21 23:01:24 +0100

    styling and slight animation with framer-motion

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import Game from "./pages/Game";
+import { AnimatePresence } from "framer-motion";
 
 function App() {
+  const location = useLocation();
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/game" element={<Game />} />
-    </Routes>
+    <AnimatePresence>
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Home />} />
+        <Route path="/game" element={<Game />} />
+      </Routes>
+    </AnimatePresence>
   );
 }
 
--- src/pages/Game/index.js
+++ src/pages/Game/index.js
@@ -2,17 +2,23 @@ import React from "react";
 import { Button, LevelDisplay, ScoreInfo } from "../../features";
 import GameBoard from "../../features/gameBoard";
 import "./index.scss";
+import { motion } from "framer-motion";
 
 const Game = () => {
   return (
-    <div className="app__game">
+    <motion.div
+      className="app__game"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
       <section>
         <LevelDisplay />
         <ScoreInfo />
         <Button shuffle={true} />
       </section>
       <GameBoard />
-    </div>
+    </motion.div>
   );
 };
 
