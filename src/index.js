c6aab699fe4ba7a9f8b1b912ab2653e821ce9255
Vania Tavares <vntavares56@gmail.com>
2022-04-24 22:43:14 +0100

    styling adjustments

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import { store } from "./app/store";
@@ -11,9 +10,7 @@ const container = document.getElementById("root");
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App tab="home" />
-    </BrowserRouter>
+    <App tab="home" />
   </Provider>
 );
 
